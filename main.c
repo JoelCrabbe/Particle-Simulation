66b83de59c4669e89cbf9db42d59a521b0c23295
Joel Crabbe <joel.crabbe@outlook.com>
2025-01-15 15:04:30 +0000

    collisions with window walls works, currently working on particle collisions, still working in 2D

--- main.c
+++ main.c
@@ -1,9 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "raylib.h"
+#include "raymath.h"
+
 
 #define SCREEN_HEIGHT 800
 #define SCREEN_WIDTH 800
+
  
 
 typedef struct{
@@ -16,11 +19,17 @@ typedef struct{
 }Particle;
 
 //TODO Need to find out how to get the coordinates of each of the walls of the window
+//I think the top and bottom coordinates of the wall in 2d are y = +- 22.50 because fovy is centered at (0,0,0) and we have half the fov in each direction
 int check_position(Particle* particle){
+    //this handles collisions of the particles with the top and bottom edge of the window
+    if(particle->position.y - particle->radius <= -22.5f || particle->position.y + particle->radius >= 22.50f){
+        particle->velocity.y *= -1;
+    }
+    else if(particle->position.x + particle->radius >= 22.50f || particle->position.x - particle->radius <= -22.50f){
+        particle->velocity.x *= -1;
+    }
 
 
-
-  
 }
 
 int update_position(Particle* particle){
@@ -44,43 +53,85 @@ int update_acceleration(Particle* particle){
 
 }
 
-int gravitational_force(Particle* p1, Particle* p2){
+int check_collisions(Particle* p1, Particle* p2){
+    //calculate the distance between the two particles centers
+    Vector3 vector = Vector3Subtract(p1->position, p2->position);
+    float distance = (float) sqrt(pow(vector.x, 2) + pow(vector.y, 2) + pow(vector.z, 2));
+    if(distance <= p1->radius + p2->radius){
+        //if the particles have collided we need to nudge them a little bit
+        //we need to move the particles away from each other along a line which passes through both their centres
+        Vector3 a = p1->position;
+        float alpha = 5.0f;
+        Vector3 r = Vector3Add(a, Vector3Scale(vector, alpha));
+        //so r is the vector equation of the line passing through the centers of p1 and p2
+        Vector3 origin = {0.0f,0.0f,0.0f};
+        DrawLine3D(r, origin, BLACK);
 
+    }
 
 }
+
 
 int main(void){
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "Particle Simulation");
 
     //                   position         velocity         acceleration      radius
-    Particle p1 = {{0.0f,15.0f,0.0f}, {1.0f,-1.0f,1.0f}, {1.0f,-4.905f,1.0f}, 0.2f}; 
-    Particle p2 = {{2.0f,15.0f,0.0f}, {2.0f,-2.0f,2.0f}, {2.0f,-4.905f,2.0f}, 0.2f};
-    Particle p3 = {{4.0f,15.0f,0.0f}, {3.0f,-3.0f,3.0f}, {3.0f,-4.905f,3.0f}, 0.2f};
+    Particle p1 = {{10.0f,15.0f,0.0f}, {4.0f,1.0f,0.0f}, {4.0f,-4.905f,0.0f}, 0.2f}; 
+    Particle p2 = {{-12.0f,-15.0f,0.0f}, {8.0f,-4.0f,0.0f}, {3.0f,-4.905f,0.0f}, 0.2f};
+    Particle p3 = {{4.0f,0.0f,0.0f}, {1.0f,-4.0f,0.0f}, {-2.0f,-4.905f,0.0f}, 0.2f};
+
+    //shader settings
         
     //camera settings
+    /*
+    Camera camera = { 0 };
+    camera.position = (Vector3){ 10.0f, 10.0f, 10.0f }; // Camera position
+    camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };      // Camera looking at point
+    camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };          // Camera up vector (rotation towards target)
+    camera.fovy = 45.0f;                                // Camera field-of-view Y
+    camera.projection = CAMERA_ORTHOGRAPHIC;             // Camera projection type
+    */
     
     Camera3D camera = {0};
     camera.position = (Vector3){ 0.0f, 0.0f, 10.0f }; // Camera position in 3D space
     camera.target = (Vector3){ 0.0f, 0.0f, 0.0f };    // Camera looks at this point
     camera.up = (Vector3){ 0.0f, 1.0f, 0.0f };        // Up vector for the camera
-    camera.fovy = 45.0f;                              // Field of view
-    camera.projection = CAMERA_ORTHOGRAPHIC;          // this camera projection makes it so particles always appear the same size
+    camera.fovy = 45.0f;                              // Field of view 22.5 degrees up and 22.5 degrees down
+    camera.projection = CAMERA_ORTHOGRAPHIC;          
+    
 
     
+
+    
+    //Vector3 cubePosition = { 0.0f, 0.0f, 0.0f };
+    //Vector2 cubeScreenPosition = { 0.0f, 0.0f };
     SetTargetFPS(60);
+
 
     while(!WindowShouldClose()){
         
         BeginDrawing(); //we want to output graphics so start drawing
-        ClearBackground(RAYWHITE);
+        ClearBackground(WHITE);
         BeginMode3D(camera);
-        
+        //UpdateCamera(&camera, CAMERA_THIRD_PERSON);
+        //cubeScreenPosition = GetWorldToScreen((Vector3){cubePosition.x, cubePosition.y + 2.5f, cubePosition.z}, camera);
+       
+
+        //DrawGrid(10, 1.0f); // the 10 controls the dimensions of the grid (10x10), the 1.0f controls how big each square is
         DrawSphere(p1.position, p1.radius, BLUE);
         DrawSphere(p2.position, p2.radius, RED);
         DrawSphere(p3.position, p2.radius, GREEN);
 
+        Vector2 right_edge = GetWorldToScreen(p1.position, camera);
 
-      
+        //printf("(%f, %f)\n", right_edge.y, p1.position.y);
+
+        //printf("(%f, %f)\n", right_edge.x, p1.position.x);
+
+
+
+    
+
 
         update_velocity(&p1);
         update_velocity(&p2);
@@ -93,6 +144,11 @@ int main(void){
         check_position(&p1);
         check_position(&p2);
         check_position(&p3);
+
+        check_collisions(&p1, &p2);
+        check_collisions(&p1, &p3);
+        check_collisions(&p2, &p3);
+
 
         
         //printf("(%f,%f,%f)\n", p1.position.x, p1.position.y, p1.position.z);
@@ -107,7 +163,6 @@ int main(void){
  
       
         
-
 
         EndMode3D();
         EndDrawing(); //ends the drawing for this particular frame then loops again
